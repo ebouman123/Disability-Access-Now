1878df4f55412a88d5936c5409eb21f4db8c5595
Evan Bouman <evan.bouman@gmail.com>
2024-12-13 13:55:05 -0600

    Fixed ./Home.css import

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,7 +1,7 @@
 import { Link } from "react-router-dom";
 import { useState, useEffect } from "react";
 import axios from "axios";
-import "./home.css";
+import "./Home.css";
 
 import { Container, Grid, Paper, Typography, Button } from "@mui/material";
 
